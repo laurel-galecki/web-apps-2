92038efaad36d5478cbd2b7e3a1ee4777391fc43
Laurel Galecki <laurel.galecki@kellogg.northwestern.edu>
2023-05-11 00:30:53 +0000

    halfway

--- app/controllers/bitcoins_controller.rb
+++ app/controllers/bitcoins_controller.rb
@@ -1,8 +1,23 @@
 class BitcoinsController < ApplicationController
 
   def index
+      # connect to the Coindesk API
+  # DON'T CHANGE THIS CODE
+  # ----------------------
+  bitcoin = params["amount"] #user-generated data
+  require "net/http"
+  require "json"
+  url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  bitcoin_data = JSON.parse(response)
+  # ----------------------
+
     # add backend code
     # render bitcoins/index view
+    @usd_rate = bitcoin_data["bpi"]["USD"]["rate_float"]
+    @usd_value = @usd_rate * bitcoin.to_f
+    render :template => "bitcoins/index"
   end
   
 end
